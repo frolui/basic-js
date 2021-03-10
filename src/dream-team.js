39d9125e0415e304da68fa8efda43c043418c015
frolui <frolui29@gmail.com>
2021-03-10 20:43:36 +0300

    feat: complete dreamTeam task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let abr = [];
+
+  if (Object.prototype.toString.call(members) !== '[object Array]') {
+    return false;
+  }  
+
+  members.forEach(element => {
+      if (typeof(element) === 'string' && element !== '') {
+        abr.push(element.trim()[0].toUpperCase()); 
+      }             
+  });
+
+  return abr.sort().join('');
 };
